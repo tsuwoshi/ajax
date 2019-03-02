c7f097091b2e2f74285f349bebc87cc253cda99a
tsuwoshi <tsfender2010@gmail.com>
2019-03-03 03:12:29 +0900

    storage追加

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ var obj = $.parseJSON(recieve);
 var logoName = obj.region.img02;
 var imgList = $('.logos');
 var after = ".jpg";
+var container = $('#wrap');
 //戻り値があればコンテンツ追加
 if (obj) {
+  sessionStorage.logoName = recieve;
   for (var i = 0; i < imgList.length; i++) {
     imgList[i].src =logoName+after;
     console.log(imgList[i].src);
-    imgList.parents().removeClass('hide');
+    //要素生成
+    container.append('<div>');
+    console.log(sessionStorage.getItem('logoName'));
   }
 }else{//戻り値なければエラー画面へ遷移
 

9e9a377c569e83f28626b56907892a10b3af2607
tsuwoshi <tsfender2010@gmail.com>
2019-03-04 00:45:24 +0900

    10

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ if (obj) {
   for (var i = 0; i < imgList.length; i++) {
     imgList[i].src =logoName+after;
     console.log(imgList[i].src);
-    //要素生成
-    container.append('<div>');
     console.log(sessionStorage.getItem('logoName'));
   }
 }else{//戻り値なければエラー画面へ遷移
